c258632219d24045e222f2a0e0d1ca871ecff3eb
Karan Tandon <karan2dec@gmail.com>
2021-01-12 21:11:51 +0530

    Background is now full-width; Fixes title of add research experience

--- users/static/users/vue_components/add_research_experience.js
+++ users/static/users/vue_components/add_research_experience.js
@@ -18,7 +18,7 @@ const AddResearchExperience = {
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
-							<h5 class="modal-title" id="addReModalLongTitle">Add Education</h5>
+							<h5 class="modal-title" id="addReModalLongTitle">Add Research Experience</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
 							</button>
